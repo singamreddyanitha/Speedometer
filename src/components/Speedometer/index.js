fcf0574a3cea7b92e137550b8648d7d19f8af1ea
singamreddyanitha <127717845+singamreddyanitha@users.noreply.github.com>
2023-04-02 17:53:41 +0530

    Update index.js

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -1,11 +1,13 @@
-// Write your code here
 import {Component} from 'react'
 
 import './index.css'
 
 class Speedometer extends Component {
-  state = {speed: 0}
-  onClickBrake = () => {
+  state = {
+    speed: 0,
+  }
+
+  onClickApplyBrakeButton = () => {
     const {speed} = this.state
 
     if (speed > 0) {
@@ -13,32 +15,44 @@ class Speedometer extends Component {
     }
   }
 
-  onClickAccelerate = () => {
+  onClickAccelerateButton = () => {
     const {speed} = this.state
 
     if (speed < 200) {
       this.setState(prevState => ({speed: prevState.speed + 10}))
     }
   }
+
   render() {
     const {speed} = this.state
+
     return (
-      <div className="bgContainer">
-        <h1 className="heading">SPEEDOMETER</h1>
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
-          className="image-logo"
-          alt="speedometer"
-        />
-        <h1 className="indication">Speed is {speed}mph</h1>
-        <p className="description">Min Limit is 0mph, Max Limit is 200mph</p>
-        <div>
-          <button className="button1" onClick={this.onClickAccelerate}>
-            Accelerate
-          </button>
-          <button className="button2" onClick={this.onClickBrake}>
-            Apply Brake
-          </button>
+      <div className="speedometer-app-container">
+        <div className="speedometer-container ">
+          <h1 className="heading">SPEEDOMETER</h1>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
+            alt="speedometer"
+            className="speedometer-image"
+          />
+          <h1 className="speed-text">Speed is {speed}mph</h1>
+          <p className="speed-limits">Min Limit is 0mph, Max Limit is 200mph</p>
+          <div className="buttons-container">
+            <button
+              type="button"
+              className="accelerate-button button"
+              onClick={this.onClickAccelerateButton}
+            >
+              Accelerate
+            </button>
+            <button
+              type="button"
+              className="apply-brake-button button"
+              onClick={this.onClickApplyBrakeButton}
+            >
+              Apply Brake
+            </button>
+          </div>
         </div>
       </div>
     )
@@ -46,3 +60,4 @@ class Speedometer extends Component {
 }
 
 export default Speedometer
+
